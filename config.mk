61c9e0ef7a98afc9f029281abf05adedf19ecd99
Haziq Noor <haziq@protonmail.com>
2017-03-16 13:58:32 +0800

    Modified slstatus

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 # See LICENSE file for copyright and license details.
 
-PREFIX = /usr/local
+PREFIX = /usr
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 INCS = -I/usr/include -I${X11INC}
 LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
